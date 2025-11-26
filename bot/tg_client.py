820af8e290f774248ea68eb47c70ad0d37396a0e
suvorovDm-1 <dim-suv@mail.ru>
2025-11-26 11:09:40 +0300

    ruff fix whole project

--- bot/tg_client.py
+++ bot/tg_client.py
@@ -18,7 +18,7 @@ def makeRequest(method: str, **param) -> list[dict]:
     with urllib.request.urlopen(request) as response:
         response_body = response.read().decode("utf-8")
         response_json = json.loads(response_body)
-        assert response_json["ok"] == True
+        assert response_json["ok"]
         return response_json["result"]
 
 
