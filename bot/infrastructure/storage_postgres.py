1e55e337763895c964f47b9b7f2edebe2f5276ed
suvorovDm-1 <dim-suv@mail.ru>
2025-11-27 11:24:04 +0300

    black format

--- bot/infrastructure/storage_postgres.py
+++ bot/infrastructure/storage_postgres.py
@@ -17,7 +17,7 @@ class StoragePostgres(Storage):
         user = os.getenv("POSTGRES_USER")
         password = os.getenv("POSTGRES_PASSWORD")
         database = os.getenv("POSTGRES_DATABASE")
-        
+
         if host is None:
             raise ValueError("POSTGRES_HOST environment variable is not set")
         if port is None:
@@ -28,7 +28,7 @@ class StoragePostgres(Storage):
             raise ValueError("POSTGRES_PASSWORD environment variable is not set")
         if database is None:
             raise ValueError("POSTGRES_DATABASE environment variable is not set")
-        
+
         return pg8000.connect(
             host=host,
             port=int(port),
