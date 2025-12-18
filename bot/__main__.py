29979f14631b1a8ecd8065dc73c70a4ef0db6185
suvorovDm-1 <dim-suv@mail.ru>
2025-12-18 22:40:03 +0300

    black was successfully reformatted code

--- bot/__main__.py
+++ bot/__main__.py
@@ -12,13 +12,16 @@ dotenv.load_dotenv()
 
 dispatcher = Dispatcher()
 
+
 @dispatcher.message(F.text)
 async def message_text_echo_handler(message: Message) -> None:
     await message.answer(message.text)
 
+
 @dispatcher.message(F.photo)
 async def message_photo_echo_handler(message: Message) -> None:
     await message.answer_photo(message.photo[-1].file_id)
+
 
 @dispatcher.update.outer_middleware()
 async def database_transaction_middleware(handler: callable, event: Update, data: dict):
@@ -26,9 +29,11 @@ async def database_transaction_middleware(handler: callable, event: Update, data
     print(json.dumps(payload, ensure_ascii=False, indent=2))
     return await handler(event, data)
 
+
 async def main() -> None:
     bot = Bot(token=os.getenv("TELEGRAM_TOKEN"))
     await dispatcher.start_polling(bot)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
