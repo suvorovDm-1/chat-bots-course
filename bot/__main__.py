13b9c49f5f44e8e566a360592164de5ddaec812e
suvorovDm-1 <dim-suv@mail.ru>
2025-12-18 22:28:40 +0300

    Added middleware

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,9 +1,11 @@
 import asyncio
+import json
 import logging
 import os
 
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message
+from aiogram.types import Message, Update
+from aiogram.utils.serialization import deserialize_telegram_object_to_python
 import dotenv
 
 dotenv.load_dotenv()
@@ -17,6 +19,12 @@ async def message_text_echo_handler(message: Message) -> None:
 @dispatcher.message(F.photo)
 async def message_photo_echo_handler(message: Message) -> None:
     await message.answer_photo(message.photo[-1].file_id)
+
+@dispatcher.update.outer_middleware()
+async def database_transaction_middleware(handler: callable, event: Update, data: dict):
+    payload = deserialize_telegram_object_to_python(event)
+    print(json.dumps(payload, ensure_ascii=False, indent=2))
+    return await handler(event, data)
 
 async def main() -> None:
     bot = Bot(token=os.getenv("TELEGRAM_TOKEN"))
